abc7804a65660ccc269cbb712c6f6b640e1d4ad0
yelose <yelose85@gmail.com>
2025-08-31 18:15:16 +0200

    basic config

---
+++ src/app/features/pages/open-source/open-source.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-source',
+  imports: [],
+  templateUrl: './open-source.component.html',
+  styleUrl: './open-source.component.scss'
+})
+export class OpenSourceComponent {
+
+}
---
+++ src/app/features/pages/use-cases/use-cases.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-use-cases',
+  imports: [],
+  templateUrl: './use-cases.component.html',
+  styleUrl: './use-cases.component.scss'
+})
+export class UseCasesComponent {
+
+}
